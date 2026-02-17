a51fe863dd81e54b02bfeb4fbb188a0fdd93a5c3
teeshasingh00 <teeshasingh301@gmail.com>
2026-02-17 10:12:40 +0530

    Create multiple statement.py

---
+++ multiple statement.py
@@ -0,0 +1,15 @@
+length = int(input("enter the value of length:"))   
+breadth = int(input("enter the value of breadth:"))
+
+sum = (length + breadth) / 2
+print(sum)
+
+stationery = ["Paper", "Gel Pen", "Eraser"]
+print(stationery)
+
+first = "Mohandas"
+middle = "Karamchand"
+last = "Gandhi"
+
+fullname = first + " " + middle + " " + last
+print(fullname)
